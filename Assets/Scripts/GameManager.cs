b25930986bd8d086ceb213e88edb413bbbba897b
lkbajdp <lkbajdp@gmail.com>
2026-02-20 21:22:40 +0300

    feat: add GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,5 +2,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private SceneLoader sceneLoader;
+    private GameState currentState;
+
+    public GameState CurrentState => currentState;
+    public event System.Action<GameState> OnStateChanged;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else Destroy(gameObject);
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        currentState = newState;
+        OnStateChanged?.Invoke(newState);
+    }
 }
