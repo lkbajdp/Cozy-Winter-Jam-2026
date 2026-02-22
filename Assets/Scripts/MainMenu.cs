440e4438260e4a9c83e3f5e30a09c281a7374687
lkbajdp <lkbajdp@gmail.com>
2026-02-22 22:40:25 +0300

    feat: add main menu button logic

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,5 +2,19 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    public void PlayGameButtonClick()
+    {
+        GameManager.Instance.LoadGameScene();
+    }
+
+    public void OpenSettingButtonClick()
+    {
+        //...
+    }
+
+    public void QuitGameButtonClick()
+    {
+        Application.Quit();
+    }
     
 }
